44851a0e84afe4e19eb5426ea78d81d2e9d3fbbe
gsand1 <gsand@stu.socsd.org>
2025-02-26 09:45:24 -0500

    Create digits.java

---
+++ digits.java
@@ -0,0 +1,22 @@
+//Gabe Sand 2/25
+//This program isolates digits
+//Digits.java
+public class digits {
+
+
+    public static void main(String[]args)
+    {
+        int number;
+        int ones;
+        int tens;
+        int hundreds;
+        number = 258;
+        ones = number % 10;
+        tens = (number / 10) % 10;
+        hundreds = (number / 10) / 10 % 10;
+        
+
+
+
+    }
+}

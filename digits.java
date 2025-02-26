4fc9c01f3ad9d4695bb022d72d99dbb3b8a4ac04
gsand1 <gsand@stu.socsd.org>
2025-02-26 09:57:24 -0500

    bb

--- digits.java
+++ digits.java
@@ -10,10 +10,13 @@ public class digits {
         int ones;
         int tens;
         int hundreds;
+        int thousands;
         number = 258;
         ones = number % 10;
         tens = (number / 10) % 10;
         hundreds = (number / 10) / 10 % 10;
+        thousands = (number / 10) / 10 /10 % 10;
+        System.out.println("The ones digit is " + ones + "  the tens digit is " + tens + "  the hundreds digit is " + hundreds + " the thousands is " + thousands);
         
 
 
